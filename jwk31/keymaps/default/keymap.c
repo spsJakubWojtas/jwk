d95399f6d516a5ac3bb449a9d5dbb290df38e98f
Jakub Wojtas <wojtasjakub@sps-karvina.cz>
2024-12-05 08:27:50 +0100

    qwerty layer for DOD

--- jwk31/keymaps/default/keymap.c
+++ jwk31/keymaps/default/keymap.c
@@ -49,7 +49,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         */
         KC_ESC,    KC_P7,     KC_P8,      KC_P9,    KC_NO,     KC_NO,     KC_NO,     KC_UP,     KC_NO,     KC_BSPC,
         KC_TAB,    KC_P4,     KC_P5,      KC_P6,    KC_NO,     KC_NO,     KC_LEFT,   KC_DOWN,   KC_RGHT,   KC_ENT,
-        KC_LCTL,   KC_P1,     KC_P2,      KC_P3,    KC_NO,     KC_NO,     KC_NO,     KC_NO,     TO(0),     KC_NO,
+        KC_LCTL,   KC_P1,     KC_P2,      KC_P3,    KC_NO,     KC_NO,     KC_NO,     KC_NO,     TO(0),     TO(3),
                                                     KC_SPC
+    ),
+
+
+    [3] = LAYOUT(
+        KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,
+        KC_A,   KC_S,   KC_D,   KC_F,   KC_G,   KC_H,   KC_J,   KC_K,   KC_L,   KC_BSPC,
+        KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_N,   KC_M,   TO(0),  KC_NO,  KC_LSFT,
+                                        KC_SPC
     )
 };
